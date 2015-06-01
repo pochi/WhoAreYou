6996848f04a3f34bd4c86a9eb17fc7df04db81a8
Yuichi Kuroda <pochi.black@gmail.com>
2015-06-01 10:05:11 +0900

    Can post file to heroku

--- glass/src/main/java/com/example/pochi/whoareyou/FindPicture.java
+++ glass/src/main/java/com/example/pochi/whoareyou/FindPicture.java
@@ -5,12 +5,20 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -30,10 +38,21 @@ public class FindPicture extends AsyncTask<Uri.Builder, Void, String> {
     @Override
     protected String doInBackground(Uri.Builder... params) {
         Log.i("Asynctask", "sample");
-        HttpGet httpGet = new HttpGet("http://google-image-searcher.herokuapp.com/search?path=" + this.picturePath);
+        HttpPost httpPost = new HttpPost("http://google-image-searcher.herokuapp.com/search");
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+        File file = new File(this.picturePath);
+        FileBody fileBody = new FileBody(file);
+
+        builder.addPart("encoded_image", fileBody);
+        HttpEntity httpEntity = builder.build();
+        httpPost.setEntity(httpEntity);
+        Log.i("pochi", "garurur");
         HttpClient client = new DefaultHttpClient();
+
+
         try {
-            HttpResponse response = client.execute(httpGet);
+            HttpResponse response = client.execute(httpPost);
         } catch(ClientProtocolException ex) {
             Log.i("ClientProtocolException", "some error");
         } catch(IOException ex) {
